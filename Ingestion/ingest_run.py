612e04574a7c7bfa6b576374dd7096ad9bf2b2bc
Nathan Mwwasuku <nmwasuku@yahoo.co.uk>
2026-04-25 15:50:40 +0100

    Making script stateless by removing hardcoded values

--- Ingestion/ingest_run.py
+++ Ingestion/ingest_run.py
@@ -1,41 +1,56 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime
 import requests
 import json
 import boto3
-import yaml
 
-# API KEYS
-def keys():
-    with open("../keys.yml", "r") as f:
-        data = yaml.load(f, Loader=yaml.SafeLoader)
-    return data
+def get_parameter(name):
+    ssm = boto3.client('ssm', region_name='eu-west-2')
+    response = ssm.get_parameter(Name=name, WithDecryption=True)
+    return response['Parameter']['Value']
 
+def get_api_key():
+    client = boto3.client(
+        'secretsmanager',
+        region_name='eu-west-1'
+    )
+    response = client.get_secret_value(SecretId='strava/api_keys')
+    return json.loads(response['SecretString'])
+
+# CONNECTING TO CONNECTIONS
 def replace_access_token():
     url = "https://www.strava.com/oauth/token"
     payload = {
-        "client_id": keys()['CLIENT_ID'],
-        "client_secret": keys()['CLIENT_SECRET']
-        ,
+        "client_id": get_api_key()['CLIENT_ID'],
+        "client_secret": get_api_key()['CLIENT_SECRET'],
         "grant_type": "refresh_token",
-        "refresh_token": keys()['REFRESH_TOKEN']
+        "refresh_token": get_api_key()['REFRESH_TOKEN']
     }
     
     res = requests.post(url, data=payload)
     res = res.json()
 
-    access_token = res['access_token']
+    new_access_token = res['access_token']
+    
+    client = boto3.client(
+        'secretsmanager',
+        region_name='eu-west-1'
+    )
 
-    with open('../keys.yml', 'r') as f:
-        data = yaml.safe_load(f)
-        data['ACCESS_TOKEN'] = access_token
-    with open('keys.yml', 'w') as f:
-        yaml.dump(data, f, sort_keys=False)
+    current = get_api_key()
+    
+    # Merge updates into current secrets
+    current.update({"ACCESS_TOKEN": new_access_token})
+    
+    client.update_secret(
+        SecretId='strava/api_keys',
+        SecretString=json.dumps(current)
+    )    
 
 
 def obtain_recent_activity_ids(epoch_date):
     url = "https://www.strava.com/api/v3/athlete/activities"
     params = { "per_page": 50, "page": 1, "after": int(epoch_date) }
-    headers = { "Authorization": f"Bearer {keys()['ACCESS_TOKEN']}" }
+    headers = { "Authorization": f"Bearer {get_api_key()['ACCESS_TOKEN']}" }
     res = requests.get(url, params=params, headers=headers)
     ids = [{'ID':i.get('id'),'start_date':timestamp_to_epoch(i.get('start_date'))} for i in res.json()]
     return ids
@@ -47,32 +62,38 @@ def timestamp_to_epoch(ts_str):
     return int(dt.timestamp())
 
 def load_last_timestamp():
-    try:
-        with open("last_timestamp.yml", "r") as f:
-            data = yaml.load(f, Loader=yaml.SafeLoader)
-        return data['last_timestamp']
-    except FileNotFoundError:
-        return "2020-01-01T00:00:00Z"
+    s3 = boto3.client('s3')
+    response = s3.get_object(
+        Bucket=get_parameter("/strava/bucket_name"),
+        Key="strava/last_timestep_uploaded/timestamp.json"
+    )
+    timestamp = json.loads(response['Body'].read().decode('utf-8'))
+    return timestamp["last_timestamp"]
 
 def save_last_timestamp(timestamp):
-    data = {'last_timestamp': timestamp}
-    with open("last_timestamp.yml", "w") as f:
-        yaml.dump(data, f, sort_keys=False)
+    data = {'last_timestamp': int(timestamp)}
+    
+    s3 = boto3.client('s3')
+    
+    s3.put_object(
+        Bucket=get_parameter("/strava/bucket_name"),
+        Key=get_parameter("/strava/last_timestep_bucket"),
+        Body=json.dumps(data),
+        ContentType="application/json"
+    )
 
 def upload_strava_activity(activity_id, prefix="strava/raw"):
     url = f"https://www.strava.com/api/v3/activities/{activity_id}"
     params = {"include_all_efforts": 1}
-    headers = {"Authorization": f"Bearer {keys()['ACCESS_TOKEN']}"}
+    headers = {"Authorization": f"Bearer {get_api_key()['ACCESS_TOKEN']}"}
     res = requests.get(url, params=params, headers=headers)
     data = res.json()
-    bucket = 'strava-running-data'
     s3 = boto3.client('s3')
-    
     filename = f"activity_{activity_id}.json"
     key = f"{prefix}/{filename}"
     
     s3.put_object(
-        Bucket=bucket,
+        Bucket=get_parameter("/strava/bucket_name"),
         Key=key,
         Body=json.dumps(data),
         ContentType="application/json",
